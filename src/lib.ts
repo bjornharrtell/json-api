1450e0af75282847b5663a1f1c100324847b08a8
Björn Harrtell <bjorn@septima.dk>
2025-10-15 13:28:15 +0200

    Fix wrong type export

--- src/lib.ts
+++ src/lib.ts
@@ -14,6 +14,6 @@ export type {
   ModelDefinition,
   Relationship,
 } from './json-api.ts'
-export { type BaseRecord, RelationshipType, useJsonApi } from './json-api.ts'
+export { type BaseEntity, RelationshipType, useJsonApi } from './json-api.ts'
 export type { FetchOptions, FetchParams, JsonApiFetcher, PageOption } from './json-api-fetcher.ts'
 export { camel } from './util.ts'
