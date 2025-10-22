826f0a0fea4333ce48f6353b86a8a41563f40037
Björn Harrtell <bjorn@septima.dk>
2025-10-22 02:01:42 +0200

    Export type

--- src/lib.ts
+++ src/lib.ts
@@ -16,6 +16,6 @@ export type {
   ModelDefinition,
   Relationship,
 } from './json-api.ts'
-export { type BaseEntity, RelationshipType, useJsonApi } from './json-api.ts'
+export { type BaseEntity, type AtomicOperation, RelationshipType, useJsonApi } from './json-api.ts'
 export type { FetchOptions, FetchParams, JsonApiFetcher, PageOption } from './json-api-fetcher.ts'
 export { camel } from './util.ts'
