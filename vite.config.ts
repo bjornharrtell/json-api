95817514d85d54ada7cb203b1297e6569584f460
Björn Harrtell <bjorn@septima.dk>
2026-02-22 18:08:22 +0100

    Autoformat

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { resolve } from 'node:path'
-import { defineConfig } from 'vitest/config'
 import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vitest/config'
 
 // https://vite.dev/config/
 export default defineConfig({
